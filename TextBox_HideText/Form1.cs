91063d1f5696f36863fa234d8f6a302d0bca799a
Thế Anh <theanhnb97@users.noreply.github.com>
2018-12-21 10:43:10 +0700

    Initial commit

---
+++ TextBox_HideText/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBox_HideText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="")
+            {
+                textBox1.Text = "Nhập dữ liệu vào đây...";
+                textBox1.ForeColor = Color.Silver;
+            }
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = Color.Black;
+        }
+    }
+}
